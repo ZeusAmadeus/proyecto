25e521f4f94f26d21267993bafeb1c25f817c366
ZeusAmadeus <100966760+ZeusAmadeus@users.noreply.github.com>
2022-03-04 14:15:21 -0600

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,18 @@
+
+function setup() {
+createCanvas(400,400);
+
+  var ball = createSprite(200,200,20,20);
+  ball.shapeColor = "red";
+}
+
+function draw() 
+{
+  background(30);
+drawSprites()
+
+}
+
+
+
+
